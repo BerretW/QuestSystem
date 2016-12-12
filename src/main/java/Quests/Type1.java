5e79b2448823c70d4eba9a5bff6543f36d2dc5e6
adolf01 <adolf01@seznam.cz>
2016-12-12 20:50:23 +0100

    Added

---
+++ src/main/java/Quests/Type1.java
@@ -0,0 +1,8 @@
+package Quests;
+
+/**
+ * Created by Alois on 12.12.2016.
+ */
+public class Type1 {
+
+}
--- src/main/java/utils/QuestUtils.java
+++ src/main/java/utils/QuestUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import org.spongepowered.api.block.tileentity.TileEntity;
+import org.spongepowered.api.entity.living.player.Player;
 
 /**
  * Created by Alois on 12.12.2016.
@@ -8,7 +9,26 @@ import org.spongepowered.api.block.tileentity.TileEntity;
 public class QuestUtils {
     TileUtils tileutils = new TileUtils();
 
-    public void getQuestType(TileEntity entity){
-        tileutils.getTileLine(entity,0);
+    public String getQuestType(TileEntity entity){
+        return tileutils.getTileLine(entity,0).get().toString();
+    }
+
+    public void runQuest(int Type, Player player, TileEntity entity){
+        switch (getQuestType(entity)) {
+            case "1":
+                runQuest1(player, entity);
+                break;
+            case "2":
+                runQuest2();
+                break;
+        }
+    }
+
+    public void runQuest1(Player player, TileEntity entity) {
+        int Level = Integer.parseInt(tileutils.getTileLine(entity, 2).get().toString());
+        
+    }
+    public void runQuest2(){
+
     }
 }
